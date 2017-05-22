c3a20ca13cf24dbd6a95b57f57dbf159cf2aa29c
Tracy Lee <tlee@inxpo.com>
2017-05-22 09:57:08 -0500

    copy changes

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div className="container">
         <div className="jumbotron">
-          <p>A simple React comment component built using the Test Driven Development (TDD) approach.</p>
+          <p>A simple React comment component built using the <a href="http://chaijs.com/api/" target="_blank">Chai</a> testing suites Test Driven Development (TDD) approach.</p>
           <p className="small">Per Wikipedia: Test-driven development (TDD) is a software development process that relies on 
             the repetition of a very short development cycle: requirements are turned into very specific test cases, 
             then the software is improved to pass the new tests, only. This is opposed to software development that 
